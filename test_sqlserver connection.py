83316b505e98a812cfffcf67f557a5796d7691bd
shenyubo <Suber0711@outlook.com>
2023-12-03 14:07:33 +0800

    modify interfaces and add annotations

--- test_sqlserver connection.py
+++ test_sqlserver connection.py
@@ -3,19 +3,22 @@ import pyodbc
 # 定义连接参数
 SERVER = '沈雨博'   # 服务器名称
 DATABASE = 'student_management_system'  # 数据库名称
-USERNAME = 'sa'   # 用户名
+USERNAME = 'shenyubo'   # 用户名
 PASSWORD = '030612'   # 密码
 
 # 构建连接字符串
 connectionString = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD}'
 print(connectionString)
 
-    # 使用 connect() 方法建立连接
+# 使用 connect() 方法建立连接
 conn = pyodbc.connect(connectionString)
 
-    # 创建游标
+# 判断是否连接成功
+print("successful connection")
+
+# 创建游标
 cursor = conn.cursor()
 
-    # 关闭游标和连接
+# 关闭游标和连接
 cursor.close()
 conn.close()
